9b66d6c3ec7df733cd34aad5eef9164032d2503d
jrafael0526 <jrafael0526@outlook.com>
2019-11-28 17:42:23 -0400

    Initial commit add mvc template

---
+++ MyBiciShop/MyBiciShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBiciShop.Startup))]
+namespace MyBiciShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
